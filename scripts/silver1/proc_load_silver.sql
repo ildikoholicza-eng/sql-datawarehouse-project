e17aa4e692db2154063bb16ad0dfeac6556b9365
Ildiko Holicza <ildiko.holicza@gmail.com>
2026-03-05 16:37:53 +0100

    Update proc_load_silver.sql

--- scripts/silver1/proc_load_silver.sql
+++ scripts/silver1/proc_load_silver.sql
@@ -1,18 +1,18 @@
 
 
+
 /*
 ============================================================================================
 
-Stored Procedure: Load Silver Layer (Bronze-> Silver)
+Stored Procedure: Load Silver Layer (Source-> Silver)
 
 ============================================================================================
 
 Script Purpose:
-	This Stored Procedure performs the ETL (Extract, Transform, Load) process to populate the 'silver'
-schema tables from the 'bronze' schema.
+	It performs the following actions:
 	- Truncates the silver tables before loading data
-	- Inserts transformed and cleansed data from Bronzebinto Silver Tables.
-	
+	- Uses the 'BULK INSERT' command to load from bronze tables into silver tables 
+	- Data is cleaned and standardized 
 
 	Parameters:
 		None.
@@ -27,8 +27,9 @@ schema tables from the 'bronze' schema.
 
 CREATE OR ALTER PROCEDURE silver.load_silver AS BEGIN
 
-	DECLARE @start_time DATETIME, @end_time DATETIME
+	DECLARE @start_time DATETIME, @end_time DATETIME ,@batch_start_time DATETIME, @batch_end_time DATETIME;
 	BEGIN TRY
+	SET @batch_start_time = GETDATE();
 	PRINT '==================================================================================';
 	PRINT 'LOADING SILVER LAYER';
 	PRINT '==================================================================================';
@@ -259,12 +260,17 @@ CREATE OR ALTER PROCEDURE silver.load_silver AS BEGIN
 	SET @end_time = GETDATE ();
 		PRINT'>> Load Duration:'+ CAST (DATEDIFF( second, @start_time, @end_time) AS VARCHAR ) + 'seconds';
 		PRINT'>>--------------------------------------------------------------------------------------<<'
+	SET @batch_end_time = GETDATE();
+	PRINT '=========================================================================================';
+	PRINT 'Loading Silver Layer is Completed';
+		PRINT '- Total Duration: '+ CAST(DATEDIFF( second, @start_time, @end_time) AS VARCHAR ) + 'seconds';
+		PRINT '========================================================================================================';
 	
 	END TRY
 
 	BEGIN CATCH 
 PRINT '==========================================================';
-PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER ';
+PRINT 'ERROR OCCURRED DURING LOADING BRONZE LAYER ';
 PRINT'Error Message' + CAST( ERROR_MESSAGE() AS NVARCHAR);
 PRINT'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
 PRINT '==========================================================';
